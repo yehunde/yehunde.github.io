a322c0fe3076ddae895603021d8f5d0190ed3b26
yehunde <gh3402166@gmail.com>
2025-12-29 14:38:55 +0800

    Site updated: 2025-12-29 14:38:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/29/Hexo博客系列CM大佬的教学/","2025/12/29/Hexo博客系列CM课程1/","2025/12/29/美女图片赵丽颖/"];function toRandomPost(){
+var posts=["2025/12/29/Hexo博客系列CM课程2/","2025/12/29/Hexo博客系列CM课程1/","2025/12/29/美女图片赵丽颖/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
